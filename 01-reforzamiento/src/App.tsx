fa1189e6e19ddc14e6ee52f4355d2b509d5a8c60
Xabier Jauregi <giamexak@gmail.com>
2021-09-23 20:04:58 +0200

    counter with custom ts hooks

--- 01-reforzamiento/src/App.tsx
+++ 01-reforzamiento/src/App.tsx
@@ -1,7 +1,9 @@
-/* import TiposBasicos from "./typescript/TiposBasicos" */
-/* import ObjetosLiterales from "./typescript/ObjetosLiterales" */
-
+/* import TiposBasicos from "./typescript/TiposBasicos"
+import ObjetosLiterales from "./typescript/ObjetosLiterales"
 import Funciones from "./typescript/Funciones"
+import Contador from "./components/Contador"
+import ContadorConHook from "./components/ContadorConHook" */
+import Login from './components/Login';
 
 const App = () => {
   return (
@@ -10,7 +12,10 @@ const App = () => {
       <hr />
       {/* <TiposBasicos /> */}
       {/* <ObjetosLiterales /> */}
-      <Funciones/>
+      {/* <Funciones/> */}
+      {/* <Contador/> */}
+      {/* <ContadorConHook /> */}
+      <Login />
     </div>
   )
 }
