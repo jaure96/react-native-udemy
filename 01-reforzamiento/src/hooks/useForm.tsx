bbc35769644d8a081b285fb2629802f80721c4c8
Xabier Jauregi <giamexak@gmail.com>
2021-09-27 22:41:15 +0200

    useForm

---
+++ 01-reforzamiento/src/hooks/useForm.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+
+const useForm = <T extends Object>(formulario: T) => {
+
+    const [state, setState] = useState(formulario)
+
+    const onChange = (value: string, campo: keyof T) => {
+        setState({
+            ...state,
+            [campo]: value
+        })
+    }
+
+    return {
+        ...state,
+        formulario: state,
+        onChange
+    }
+
+}
+
+export default useForm
