3394fa1ec9ac9dfdc89f5b27c3f2f44c366dbc13
Xabier Jauregi <giamexak@gmail.com>
2021-11-24 19:09:13 +0100

    create routes app

---
+++ 08-RutasApp/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+const App = () => {
+  return (
+    <>
+      <Icon name='star' />
+    </>
+  )
+}
+
+export default App
