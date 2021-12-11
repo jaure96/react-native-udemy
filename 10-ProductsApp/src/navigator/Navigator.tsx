b49f5e394bbb312557d0ea7a8cffc5e4ebb3e780
Xabier Jauregi <giamexak@gmail.com>
2021-12-11 12:45:18 +0100

    create products navigator

--- 10-ProductsApp/src/navigator/Navigator.tsx
+++ 10-ProductsApp/src/navigator/Navigator.tsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
-import ProtectedScreen from '../screens/ProtectedScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import { AuthContext } from '../context/AuthContext';
 import LoadingScreen from '../screens/LoadingScreen';
+import ProductsNavigator from './ProductsNavigator';
 
 const Stack = createStackNavigator();
 
@@ -29,7 +29,7 @@ const Navigator = () => {
                     <Stack.Screen name="LoginScreen" component={LoginScreen} />
                     <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
                 </>
-                : <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
+                : <Stack.Screen name="ProductsNavigator" component={ProductsNavigator} />
             }
 
 
